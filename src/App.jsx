7d67cb35da325fae5c9ea28f93e624a03d73b1b9
Thomasplie <1039350@hr.nl>
2026-03-28 22:52:15 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+    return (
+        <>
+            <div className="flex h-[100vh] w-full justify-center items-center">
+                <h1 className="text-4xl text-green-300">
+                    Hello world
+                </h1>
+            </div>
+        </>
+    )
+}
+
+export default App
